01026b7f11077606bb11b6c31073297dfdeb0771
harshal <kardileharshal567@gmail.com>
2023-08-16 19:38:24 +0530

    completed till Cleaner code

--- functions.php
+++ functions.php
@@ -1,5 +1,33 @@
 <?php 
     
+    function pageBanner($args = NULL) {
+  
+        if (!isset($args['title'])) {
+          $args['title'] = get_the_title();
+        }
+       
+        if (!isset($args['subtitle'])) {
+          $args['subtitle'] = get_field('page_banner_subtitle');
+        }
+       
+        if (!isset($args['photo'])) {
+          if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
+            $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+          } else {
+            $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+          }
+        }
+?>
+        <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+            <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?php echo $args['title']?></h1>
+                <div class="page-banner__intro">
+                <p><?php echo $args['subtitle'];?></p>
+                </div>
+            </div>
+            </div>
+  <?php  }
     function university_files() {
         wp_enqueue_style("university_main_styles", get_theme_file_uri("build/style-index.css"));
         wp_enqueue_style("university_extra_styles", get_theme_file_uri("build/index.css"));
@@ -16,12 +44,24 @@
         // register_nav_menu('footerLocationTwo', 'footer Location Two');
 
         add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+        add_image_size('professorLandscape', 400, 260, true);
+        add_image_size('professorPortrait', 480, 650, true);
+       add_image_size('pageBanner', 1500, 350, true);
+        
     }
 
     add_action("after_setup_theme", "university_features");
 
     
     function university_adjust_queries($query){
+
+        if(!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()){
+            $query->set('orderby','title');
+            $query->set('order', 'ASC');
+            $query->set('posts_per_page', -1);
+        }
+
         if(!is_admin()  AND is_post_type_archive('event') AND $query->is_main_query()){
             $today = date('Ymd');
             $query->set( 'meta_key', 'event_date');
--- single-professor.php
+++ single-professor.php
@@ -3,29 +3,33 @@
 get_header();
 
 while(have_posts()){
-    the_post();
-    pageBanner();
-    ?>
-   
 
+    the_post();
+    
+    pageBanner();
+
+    ?>
+    
         <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-          <p>
-            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>">
-            <i class="fa fa-home" aria-hidden="true"></i>
-            Event Home </a>
-            <span class="metabox__main"><?php the_title(); ?></span>
-          </p>
-        </div>
-            <div class ="generic-content"><?php the_content();?>
-            </div>  
+        
+            <div class ="generic-content">
+                <div class="row group">
+                    <div class="one-third">
+                    <?php the_post_thumbnail('professorPortrait');?>    
+                    </div>
+                    <div class="two-third">
+                    <?php the_content(); ?>
+                    </div>    
+                </div>
+            </div>
+       
             <?php
             $relatedPrograms = get_field('related_programs');
 
             if($relatedPrograms){
             
             echo '<hr class="section-break">';
-            echo '<h2 class="headline headline--medium"> Related Programs(s)</h2>';
+            echo '<h2 class="headline headline--medium"> Subject(s) Taught</h2>';
             echo '<ul class="link-list min-list">';
             foreach($relatedPrograms as $program){ ?>
             <li><a href="<?php echo get_the_permalink($program); ?>">
